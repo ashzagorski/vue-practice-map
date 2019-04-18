d3d221255b0033d06bdb5556e6f1fbc092cff0d3
ashzagorski <ash.zagorski@gmail.com>
2019-04-17 20:58:15 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
     </div>
     <router-view/>
   </div>
@@ -16,16 +18,30 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+.topnav {
+  background-color: #333;
+  overflow: hidden;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
 }
 </style>

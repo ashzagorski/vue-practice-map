bb1a335dbeae56ecb7561f718a570dc8dc95fa30
ashzagorski <ash.zagorski@gmail.com>
2019-05-06 17:02:48 -0500

    attempt map

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,21 +13,23 @@
 
 </template>
 <script>
+  
+  import GoogleMap from './GoogleMap.vue'
 export default {
   
   name: 'google-map',
   props: ['name'],
-  data: function () {
+  data: function() {
     return {
       mapName: this.name + "-map",
-    }
+    };
   },
   mounted: function() {
     const element = document.getElementById(this.mapName);
     const options = {
       zoom: 14,
       center: new google.maps.LatLng(51.501527,-0.1921837)
-    }
+    };
     const map = new google.maps.Map(element, options);
   }
 };
